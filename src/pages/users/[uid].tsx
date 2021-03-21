58de540a0d8b10703c204f4584ba89d1b3a64317
keisukesaito7 <keisukesaito7@gmail.com>
2021-03-21 21:37:21 +0900

    一旦コミット

--- src/pages/users/[uid].tsx
+++ src/pages/users/[uid].tsx
@@ -45,7 +45,30 @@ const UserShow: React.FC = () => {
   }, [query.uid])
 
   // defaultUserが表示されるので、nameがからのときに
-  return <div>{user.name.length ? user.name : 'now loading ...'}</div>
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg navbar-light mb-3"
+        style={{ backgroundColor: '#e3f2fd' }}
+      >
+        <div className="container">
+          <div className="mr-auto">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+          </div>
+          <form className="d-flex">
+            <button className="btn btn-outline-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <div className="container">
+        <div>{user.name.length ? user.name : 'now loading ...'}</div>
+      </div>
+    </div>
+  )
 }
 
 export default UserShow
