432d2f99953d7356ea3248aae39463e839612f4d
keisukesaito7 <keisukesaito7@gmail.com>
2021-03-21 21:28:11 +0900

    scssファイルの再配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { RecoilRoot } from 'recoil'
 import '../libs/firebase'
+import '../styles/index.scss'
+import '../styles/globals.scss'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }) {
