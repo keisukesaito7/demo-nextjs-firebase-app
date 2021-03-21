419316972bcd38ce2e96102a15f7c1db538e3642
keisukesaito7 <keisukesaito7@gmail.com>
2021-03-21 18:48:16 +0900

    認証設定２

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -1,34 +1,41 @@
 import firebase from 'firebase/app'
 import { User } from '../models/User'
 import { atom, useRecoilState } from 'recoil'
+import { useEffect } from 'react'
 
 const userState = atom<User>({
   key: 'user',
   default: null
 })
 
-export function useAuthentication() {
+export function useAuthentication(): { user: User } {
   const [user, setUser] = useRecoilState(userState)
 
-  firebase
-    .auth()
-    .signInAnonymously()
-    .catch(function (error) {
-      // eslint-disable-next-line no-console
-      console.log(error)
-    })
-
-  firebase.auth().onAuthStateChanged(function (firebaseUser) {
-    if (firebaseUser) {
-      setUser({
-        uid: firebaseUser.uid,
-        isAnonymous: firebaseUser.isAnonymous
-      })
-    } else {
-      // User is signed out.
-      setUser(null)
+  useEffect(() => {
+    if (user !== null) {
+      return
     }
-  })
+
+    firebase
+      .auth()
+      .signInAnonymously()
+      .catch(function (error) {
+        // eslint-disable-next-line no-console
+        console.log(error)
+      })
+
+    firebase.auth().onAuthStateChanged(function (firebaseUser) {
+      if (firebaseUser) {
+        setUser({
+          uid: firebaseUser.uid,
+          isAnonymous: firebaseUser.isAnonymous
+        })
+      } else {
+        // User is signed out.
+        setUser(null)
+      }
+    })
+  }, [])
 
   return { user }
 }
